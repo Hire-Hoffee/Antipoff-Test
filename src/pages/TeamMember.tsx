482117af726aef1e2ff6b2c4eb2044d3406f5412
Rostislav Alekseev <mr.rostik.01@gmail.com>
2024-06-16 15:23:53 +0200

    member page header style

--- src/pages/TeamMember.tsx
+++ src/pages/TeamMember.tsx
@@ -1,9 +1,30 @@
-import React from "react";
+import styles from "@/styles/TeamMember.module.css";
+import exit from "@/assets/exit.svg";
 
 type Props = {};
 
 function TeamMember({}: Props) {
-  return <div>TeamMember</div>;
+  return (
+    <>
+      <header className={styles.header}>
+        <div className={styles.headerContent}>
+          <img src="https://i.pravatar.cc/124" alt="avatar" />
+          <div>
+            <h1>Antipoff</h1>
+            <span>Партнер</span>
+          </div>
+        </div>
+        <button className={styles.exit_btn}>
+          <span>Выход</span>
+          <img src={exit} alt="icon" />
+        </button>
+        <button className={styles.back_btn}>
+          <span>Назад</span>
+        </button>
+      </header>
+      <main className={styles.main}>main</main>
+    </>
+  );
 }
 
 export default TeamMember;
