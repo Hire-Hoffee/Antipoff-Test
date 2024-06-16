6637356682a29190503c4eea359519e207bf5500
Rostislav Alekseev <mr.rostik.01@gmail.com>
2024-06-16 13:57:43 +0200

    header styling

--- src/pages/TeamList.tsx
+++ src/pages/TeamList.tsx
@@ -1,9 +1,24 @@
 import React from "react";
+import styles from "@/styles/TeamList.module.css";
 
 type Props = {};
 
 function TeamList({}: Props) {
-  return <div>TeamList</div>;
+  return (
+    <>
+      <header className={styles.header}>
+        <div className={styles.headerContent}>
+          <h1>Наша команда</h1>
+          <p>
+            Это опытные специалисты, хорошо разбирающиеся во всех задачах, которые ложатся на их
+            плечи, и умеющие находить выход из любых, даже самых сложных ситуаций.
+          </p>
+        </div>
+        <button className={styles.exit_btn}>Выход</button>
+      </header>
+      <main>main</main>
+    </>
+  );
 }
 
 export default TeamList;
