185326695b60876ec501dc2d20afcec3da7eafaa
Rostislav Alekseev <mr.rostik.01@gmail.com>
2024-06-16 16:06:14 +0200

    team member description style

--- src/pages/TeamMember.tsx
+++ src/pages/TeamMember.tsx
@@ -1,5 +1,7 @@
 import styles from "@/styles/TeamMember.module.css";
 import exit from "@/assets/exit.svg";
+import phone from "@/assets/phone.svg";
+import email from "@/assets/email.svg";
 
 type Props = {};
 
@@ -22,7 +24,27 @@ function TeamMember({}: Props) {
           <span>Назад</span>
         </button>
       </header>
-      <main className={styles.main}>main</main>
+      <main className={styles.main}>
+        <div className={styles.infoContainer}>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium consequatur omnis
+            sunt dolorum doloremque, aliquid ad eum ex at perspiciatis sed numquam aut deserunt
+            pariatur ut laudantium enim libero aperiam culpa. Minus nostrum in laboriosam delectus
+            aut magnam, itaque, quidem ratione iusto, illo necessitatibus consectetur. Quo dicta
+            quisquam id cum!
+          </p>
+          <div className={styles.contactsContainer}>
+            <div>
+              <img src={phone} alt="phone" />
+              <span>+7 (954) 333-44-55</span>
+            </div>
+            <div>
+              <img src={email} alt="email" />
+              <span>sykfafkar@gmail.com</span>
+            </div>
+          </div>
+        </div>
+      </main>
     </>
   );
 }
